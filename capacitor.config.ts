0f7d24dc760d70617afe8217821a7d2378a122fd
Uyog <sean.uyoga@gmail.com>
2024-05-25 11:24:21 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'turf-scout-creator',
+  webDir: 'dist'
+};
+
+export default config;
